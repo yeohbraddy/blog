070943984a924aad8548d64a1160159444350f2e
Braddy <braddy.yeoh@ucdconnect.ie>
2020-03-26 11:47:35 +0000

    added react helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,9 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import Nav from "./Nav";
 import tachyons from "tachyons";
+import { Helmet } from "react-helmet";
 
 const Layout = ({ children }) => (
     <>
+        <Helmet>
+            <meta className="application"/>
+            <title>Braddy's Blog</title>
+        </Helmet>
         <Nav/>
         <main>{ children }</main>
     </>
